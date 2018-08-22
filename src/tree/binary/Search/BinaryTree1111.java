d55e4e455903b3d13011b7c91a851fbb7b54402e
nitin <nitinguptamca@gmail.com>
2018-08-22 19:04:17 +0530

    ss

--- src/tree/binary/Search/BinaryTree1111.java
+++ src/tree/binary/Search/BinaryTree1111.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
-public class BinaryTree {
+public class BinaryTree1111 {
 	protected Node root;
 	 // head --> Pointer to head node of created doubly linked list
     Node head;
@@ -600,22 +600,37 @@ Consider the below trees:
     }
 
 	static void main(String[] args) {
-		BinaryTree bt = new BinaryTree();
-		bt.add(6);
-		bt.add(4);
+		BinaryTree1111 bt = new BinaryTree1111();
+		bt.add(12);
+		bt.add(9);
 		bt.add(8);
+		bt.add(10);
+		bt.add(2);
+		bt.add(14);
 		bt.add(3);
+		bt.add(4);
+		bt.add(10);
+		bt.add(22);
+		bt.add(14);
+		bt.add(1);
+		bt.add(21);
+		bt.add(16);
+		bt.add(17);
+		bt.add(13);
+		bt.add(19);
+		bt.add(15);
 		bt.add(5);
 		bt.add(7);
-		bt.add(9);
-		assertTrue(bt.containsNode(6));
-		assertTrue(bt.containsNode(4));
-
+		bt.add(6);
+		bt.add(12);
+		bt.add(11);
 		bt.traverseInOrder(bt.root);
-		bt.traversePostOrder(bt.root);
+		System.out.println();
 		bt.traversePreOrder(bt.root);
+		System.out.println();
+		bt.traversePostOrder(bt.root);
+		System.out.println();
 		bt.traverseLevelOrder();
-
 		bt.findSmallestValue(bt.root);
 
 		bt.delete(5);
